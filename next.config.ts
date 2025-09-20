578fc82abb13f8ed9501782cb050f548aee9a2f1
Himanshu Bansal <himanshu.bansal@flipkart.com>
2025-09-20 11:50:10 +0000

    5:19:17 PM: FirebaseError: Firebase: Error (auth/invalid-api-key). 5:19:

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type {NextConfig} from 'next';
-import getConfig from 'next/config';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -30,15 +29,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-  },
-  publicRuntimeConfig: {
-    firebaseApiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    firebaseAuthDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    firebaseProjectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    firebaseStorageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    firebaseMessagingSenderId:
-      process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    firebaseAppId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   },
 };
 
